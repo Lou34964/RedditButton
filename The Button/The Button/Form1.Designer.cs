e1357fc50d6f1dd4c111dc1516c00a6ba9d5c74e
Chad <Mapletonkid@gmail.com>
2020-07-21 11:33:05 -0600

    Reddits The Button

---
+++ The Button/The Button/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace The_Button
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Purple;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(377, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(28, 51);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 10);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Value = 72;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(28, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(38, 20);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Orange;
+            this.panel2.Location = new System.Drawing.Point(65, 31);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(78, 20);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Yellow;
+            this.panel3.Location = new System.Drawing.Point(142, 31);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(119, 20);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Green;
+            this.panel4.Location = new System.Drawing.Point(259, 31);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(156, 20);
+            this.panel4.TabIndex = 6;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Blue;
+            this.panel5.Location = new System.Drawing.Point(415, 31);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(171, 20);
+            this.panel5.TabIndex = 7;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Purple;
+            this.panel6.Location = new System.Drawing.Point(586, 31);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(218, 20);
+            this.panel6.TabIndex = 8;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(795, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(833, 110);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "The Button";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ The Button/The Button/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Button
+{
+    public partial class Form1 : Form
+    {
+        //colors
+        Color White = Color.White;
+        Color red = Color.Red;
+        Color orange = Color.Orange;
+        Color yellow = Color.Yellow;
+        Color Green = Color.Green;
+        Color Blue = Color.Blue;
+        Color Purple = Color.Purple;
+
+        //interval
+        int interal = 1;
+        int statInt = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setDefault();
+            timer.Start();
+        }
+
+        private void setDefault()
+        {
+            timer.Stop();
+            timer.Enabled = false;
+            progressBar1.Value = 100;
+            this.BackColor = White;
+            button1.BackColor = Purple;
+            button1.ForeColor = White;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            iv();
+            checkProgress(progressBar1.Value);
+        }
+
+        private void iv()
+        {
+            if (statInt > 0)
+            {
+                statInt -= 1;
+            }
+            else
+            {
+                progressBar1.Value -= 1;
+                statInt = interal;
+            }
+        }
+
+        private void checkProgress(int p)
+        {
+            if (p > 72)
+            {
+                button1.BackColor = Purple;
+                button1.ForeColor = White;
+            }
+            else if(p > 50)
+            {
+                button1.BackColor = Blue;
+                button1.ForeColor = White;
+            }
+            else if (p > 30)
+            {
+                button1.BackColor = Green;
+                button1.ForeColor = Color.Black;
+            }
+            else if (p > 15)
+            {
+                button1.BackColor = yellow;
+                button1.ForeColor = Color.Black;
+            }
+            else if (p > 5)
+            {
+                button1.BackColor = orange;
+                button1.ForeColor = Color.Black;
+            }
+            else
+            {
+                button1.BackColor = red;
+                button1.ForeColor = White;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BackColor = button1.BackColor;
+            Saved();
+        }
+
+        private void Saved()
+        {
+            timer.Stop();
+            timer.Enabled = false;
+            progressBar1.Value = 100;
+            button1.BackColor = Purple;
+            button1.ForeColor = White;
+            interal = 1;
+            statInt = 1;
+            timer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to The Button. This is a simulation of the Redit button from 2015.\nThe progress bar will slowly move down and when it hit the next color the click button will change color.\nClicking the button will give you the status color of when you pressed it, then the progess bar will reset.\nHave fun!");
+        }
+    }
+}
